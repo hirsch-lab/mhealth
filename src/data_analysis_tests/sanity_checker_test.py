83ed361bbfa626c3039ad585a7d3bd2de24882a7
Norman <normanius@gmail.com>
2020-12-18 18:48:36 +0100

    fixes after merging main into this feature branch

--- src/data_analysis_tests/sanity_checker_test.py
+++ src/data_analysis_tests/sanity_checker_test.py
@@ -5,16 +5,18 @@ import pandas as pd
 
 from data_analysis.sanity_checker import SanityChecker
 
+_MHEALTH_DATA = os.getenv('MHEALTH_DATA', '../resources')
+
 
 class SanityCheckerTest(unittest.TestCase):
     checker = SanityChecker()
 
     def test_run_vital(self):
-        in_dir = '../resources/vital_signals/'
+        in_dir = f'{_MHEALTH_DATA}/vital_signals/'
         out_file = 'sanity-test-vital.csv'
         self.checker.run_vital(in_dir, 30, '_storage-sig.csv', out_file)
 
-        df = pd.read_csv(os.path.join('../resources/', out_file))
+        df = pd.read_csv(os.path.join(f'{_MHEALTH_DATA}/', out_file))
         self.check_signal_float(df, 'hours', 0.008333333, 0.009444444, 61.86916667)
         self.check_signal_float(df, '#ValidRows', 5, 14, 47825)
         self.check_signal_float(df, 'HR_mean', 44.66666667, 40.55172414, 55.11635642)
@@ -23,10 +25,10 @@ class SanityCheckerTest(unittest.TestCase):
         self.check_signal_float(df, 'HR_max', 81, 86, 74)
 
     def test_run_mixed_raw_vital(self):
-        in_dir = '../resources/mixed_vital_raw_signals/'
+        in_dir = f'{_MHEALTH_DATA}/mixed_vital_raw_signals/'
         out_file = 'sanity-test-mixed-raw-vital.csv'
         self.checker.run_mixed_raw_vital(in_dir, 30, '_Test_mixed_raw.csv', out_file)
-        df = pd.read_csv(os.path.join('../resources/', out_file))
+        df = pd.read_csv(os.path.join(f'{_MHEALTH_DATA}/', out_file))
         self.check_signal_float(df, 'hours', 73.04111111, 0.004722222, 0.004722222)
         self.check_signal_float(df, '#ValidRows', 149, 18, 18)
         self.check_signal_float(df, 'HR_mean', 79.04697987, 77.88888889, 69.94444444)
