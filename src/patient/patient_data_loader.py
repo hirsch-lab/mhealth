b3bbbb269a7400eaf8d5e3663aaf5ed5868dac2c
susuter <sues@zhaw.ch>
2020-12-18 13:20:42 +0100

    imove: preprocess raw data

--- src/patient/patient_data_loader.py
+++ src/patient/patient_data_loader.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class PatientDataLoader:
 
-    def load_everion_patient_data(self, dir_name, filename, csv_delimiter, tz_to_zurich=True):
+    def load_everion_patient_data(self, dir_name, filename, csv_delimiter, tz_to_zurich=True, drop_first_row=False):
         print("loading everion data from file " + filename + " ...")
 
         csv_in_file = os.path.join(dir_name, filename)
@@ -14,6 +14,8 @@ class PatientDataLoader:
             return pd.DataFrame()
 
         df = pd.read_csv(csv_in_file, sep=csv_delimiter)
+        if drop_first_row:
+            df.drop(df.head(1).index, inplace=True)
 
         if tz_to_zurich:
             df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_convert('Europe/Zurich')
