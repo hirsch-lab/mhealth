eb515eca558733b1470aee34af2b98bae7584096
Norman <normanius@gmail.com>
2021-01-12 00:33:11 +0100

    Refactor FileHelper.get_out_dir() - the central bit to separate source from output data in the tests

--- src/utils/file_helper.py
+++ src/utils/file_helper.py
@@ -1,16 +1,21 @@
-import os
 from pathlib import Path
 
 
 class FileHelper:
 
     @staticmethod
-    def get_out_dir(in_dir, out_dir_suffix):
-        out_dir_name = Path(in_dir).name + out_dir_suffix
-        out_dir = os.path.join(os.path.join(in_dir, os.pardir), out_dir_name)
+    def get_out_dir(in_dir, out_dir, out_dir_suffix=""):
+        """
+        Construct and create output directory:
+            out_dir = out_dir / (in_dir.name+out_dir_suffix)
+        """
+        in_dir = Path(in_dir)
+        out_dir = Path(out_dir)
+        out_dir_name = in_dir.name + out_dir_suffix
+        out_dir = out_dir / out_dir_name
 
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
+        if not out_dir.is_dir():
+            out_dir.mkdir(parents=True)
 
         return out_dir
 
