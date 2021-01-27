c7d6a405ab997b984bcbc50a74d191fb9be15426
Norman <normanius@gmail.com>
2021-01-27 15:54:48 +0100

    Refactor quality_filter - separate filter functionality from io operations

--- src/data_analysis/quality_filter.py
+++ src/data_analysis/quality_filter.py
@@ -6,7 +6,59 @@ import pandas as pd
 from patient.patient_data_loader import PatientDataLoader
 
 
+# All operations modify input df in-place!
+
+def set_bad_quality_nan_range(df, min_quality, signal_name, range):
+    signal_quality_name = signal_name + '_quality'
+    filter_quality_range(df, min_quality, range, signal_quality_name)
+
+def filter_quality_range(df, min_quality, range, signal_quality_name):
+    assert False, "Deprecated. Use filter_quality_except() instead."
+    df.iloc[df[signal_quality_name] < min_quality, range] = np.nan
+    df.iloc[df[signal_quality_name] > 100, range] = np.nan
+
+def filter_quality_except(df, min_quality, except_cols, signal_quality_name):
+    col_mask = ~df.columns.isin(except_cols)
+    df.loc[df[signal_quality_name] < min_quality, col_mask] = np.nan
+    df.loc[df[signal_quality_name] > 100, col_mask] = np.nan
+
+def set_bad_quality_nan(df, min_quality, signal_name):
+    signal_quality_name = signal_name + '_quality'
+    filter_quality(df, min_quality, signal_name, signal_quality_name)
+
+def filter_quality(df, min_quality, signal_name, signal_quality_name):
+    df.loc[df[signal_quality_name] < min_quality, signal_name] = np.nan
+    df.loc[df[signal_quality_name] > 100, signal_name] = np.nan
+
+def filter_bad_quality_vital(df, min_quality):
+    set_bad_quality_nan(df, min_quality, 'core_temperature')
+    set_bad_quality_nan(df, min_quality, 'oxygen_saturation')
+    set_bad_quality_nan(df, min_quality, 'activity_classification')
+    set_bad_quality_nan(df, min_quality, 'energy')
+    set_bad_quality_nan(df, min_quality, 'heart_rate_variability')
+    set_bad_quality_nan(df, min_quality, 'respiration_rate')
+
+    # quality signals omitted to remember original quality
+    set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(0,18))
+    set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(20,22))
+    set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(27,29))
+
+def filter_bad_quality_mixed_vital_raw(df, min_quality):
+    """
+    Filter data selected by everion_keys.MIX
+    """
+    filter_quality(df, min_quality, 'Classification', 'QualityClassification')
+    filter_quality(df, min_quality, 'SPo2', 'SPO2Q')
+
+    # Quality signals omitted to remember original quality.
+    quality_cols = ["HRQ", "SPo2Q", "QualityClassification"]
+    filter_quality_except(df, min_quality, quality_cols, 'HRQ')
+
+
 class QualityFilter:
+    """
+    Legacy
+    """
     loader = PatientDataLoader()
 
     def filter_bad_quality_vital(self, in_dir, out_dir, in_file_name, min_quality):
@@ -21,17 +73,8 @@ class QualityFilter:
         if df.empty:
             return
 
-        self.set_bad_quality_nan(df, min_quality, 'core_temperature')
-        self.set_bad_quality_nan(df, min_quality, 'oxygen_saturation')
-        self.set_bad_quality_nan(df, min_quality, 'activity_classification')
-        self.set_bad_quality_nan(df, min_quality, 'energy')
-        self.set_bad_quality_nan(df, min_quality, 'heart_rate_variability')
-        self.set_bad_quality_nan(df, min_quality, 'respiration_rate')
-
-        # quality signals omitted to remember original quality
-        self.set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(0,18))
-        self.set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(20,22))
-        self.set_bad_quality_nan_range(df, min_quality, 'heart_rate', range(27,29))
+        # In-place.
+        filter_bad_quality_vital(df, min_quality)
 
         df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_convert('UTC')
         df.to_csv(csv_out_file, sep=';')
@@ -48,31 +91,9 @@ class QualityFilter:
         if df.empty:
             return
 
-        self.filter_quality(df, min_quality, 'Classification', 'QualityClassification')
-        self.filter_quality(df, min_quality, 'SPo2', 'SPO2Q')
-
-        # quality signals omitted to remember original quality
-        self.filter_quality_range(df, min_quality, range(0,1), 'HRQ')
-        self.filter_quality_range(df, min_quality, range(2,3), 'HRQ')
-        self.filter_quality_range(df, min_quality, range(4,8), 'HRQ')
-        self.filter_quality_range(df, min_quality, range(9, 19), 'HRQ')
-
+        # In-place.
+        filter_bad_quality_mixed_vital_raw(df, min_quality)
 
         df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_convert('UTC')
         df.to_csv(csv_out_file, sep=';')
 
-    def set_bad_quality_nan_range(self, df, min_quality, signal_name, range):
-        signal_quality_name = signal_name + '_quality'
-        self.filter_quality_range(df, min_quality, range, signal_quality_name)
-
-    def filter_quality_range(self, df, min_quality, range, signal_quality_name):
-        df.iloc[df[signal_quality_name] < min_quality, range] = np.nan
-        df.iloc[df[signal_quality_name] > 100, range] = np.nan
-
-    def set_bad_quality_nan(self, df, min_quality, signal_name):
-        signal_quality_name = signal_name + '_quality'
-        self.filter_quality(df, min_quality, signal_name, signal_quality_name)
-
-    def filter_quality(self, df, min_quality, signal_name, signal_quality_name):
-        df.loc[df[signal_quality_name] < min_quality, signal_name] = np.nan
-        df.loc[df[signal_quality_name] > 100, signal_name] = np.nan
