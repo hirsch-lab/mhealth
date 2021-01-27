af8c8f21ca13b921a87f108a5045ab141e5b71bd
Norman <normanius@gmail.com>
2021-01-27 15:55:59 +0100

    utils.file_helper: fix a bug

--- src/utils/file_helper.py
+++ src/utils/file_helper.py
@@ -16,7 +16,7 @@ class FileHelper:
             get_out_dir(out_dir=path, out_dir_suffix="suffix")
         """
         assert in_dir is not None or out_dir is not None
-        in_dir = Path(out_dir) if in_dir is None else Path(in_dir)
+        in_dir = Path("") if in_dir is None else Path(in_dir)
         out_dir = Path(in_dir) if out_dir is None else Path(out_dir)
         out_dir_name = in_dir.name + out_dir_suffix
         out_dir = out_dir / out_dir_name
