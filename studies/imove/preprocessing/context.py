67ef2a80666c08e27f24bd9e4bb9306679459e7b
Norman <normanius@gmail.com>
2021-03-26 01:03:11 +0100

    Move files addressing the preprocessing into a common subdirectory

--- studies/imove/preprocessing/context.py
+++ studies/imove/preprocessing/context.py
@@ -2,5 +2,5 @@ import sys
 from pathlib import Path
 
 dir_path = Path(__file__).parent
-src_path = (dir_path / ".." / ".." / "src").resolve()
+src_path = (dir_path / ".." / ".." / ".." / "src").resolve()
 sys.path.insert(0, str(src_path))
