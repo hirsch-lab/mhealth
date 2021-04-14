c95a310706ae270de9426a14def3e019256f4d07
Norman <normanius@gmail.com>
2021-04-14 17:59:08 +0200

    visualization.vis_properties: fix problem problem with type hints

--- src/mhealth/visualization/vis_properties.py
+++ src/mhealth/visualization/vis_properties.py
@@ -6,14 +6,14 @@ from ..utils.data_aggregator import Normalization
 
 @dataclass
 class VisProperties:
-    in_dir: string
-    out_dir: string
-    normalization: Normalization.NONE
+    in_dir: str
+    out_dir: str
     keys: dict
     min_scale: float
     max_scale: float
     start_idx: int
     end_idx: int
     short_keys: dict
-    colormap: string = 'coolwarm'
+    colormap: str = "coolwarm"
     fill_hours: bool = True
+    normalization: Normalization=Normalization.NONE
