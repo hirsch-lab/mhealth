fd36932447b7a2c1113884f2be96f0886c3f51c6
Norman <normanius@gmail.com>
2021-04-20 02:51:17 +0200

    mhealth.patient: add some speedup to add_label(), but there’s still room for improvement

--- src/mhealth/patient/imove_label_loader.py
+++ src/mhealth/patient/imove_label_loader.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from pathlib import Path
 from natsort import natsort
@@ -70,18 +71,35 @@ def merge_labels(df, df_labels):
         df_labels.apply(lambda row: add_label(row, df), axis=1)
     return df
 
-
 def add_label(label_row, df):
+    def get_item(df, key):
+        # Get column or index.
+        # https://github.com/pandas-dev/pandas/issues/41038
+        if key in df:
+            return df[key]
+        elif key in df.index.names:
+            return df.index.get_level_values(key)
+        else:
+            return None
     start = label_row["StartDate"]
     end = label_row["EndDate"]
-    sel = (df.timestamp >= start) & (df.timestamp <= end)
-    df.loc[sel, "DeMorton"] = True
+    timestamp = get_item(df, key="timestamp")
+    if timestamp.is_monotonic_increasing:
+        # Speed
+        imin = timestamp.searchsorted(start, side="left")
+        imax = timestamp.searchsorted(end, side="right")
+        mask = np.zeros(len(df), dtype=bool)
+        mask[imin:imax] = True
+    else:
+        # Fall-back: classic approach
+        mask2 = ((timestamp >= start) & (timestamp <= end))
+    # FIXME: Most of the time (50-80%) is spent in the following lines.
+    df.loc[mask, "DeMorton"] = True
     if "Task" in label_row or True:
         # Task is always available!
         label = label_row["Task"]
-        df.loc[sel, "DeMortonLabel"] = label
+        df.loc[mask, "DeMortonLabel"] = label
     if "Day" in label_row:
         # Day is added optionally by calling script.
         day = label_row["Day"]
-        df.loc[sel, "DeMortonDay"] = day
-
+        df.loc[mask, "DeMortonDay"] = day
