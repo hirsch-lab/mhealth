85598bd5ed6afd9d06d7dbac8f7060ae62d4a371
Julien Biland <julianbiland@gmail.com>
2022-02-04 22:08:50 +0100

    minor changes

--- studies/imove/analysis/acceleration/RUN_2_features.py
+++ studies/imove/analysis/acceleration/RUN_2_features.py
@@ -14,16 +14,17 @@ from demmi_ex_dict import demmi_ex
 from acceleration import put_margins_around_ex, resample, align_timestamp
 from feature_dev import feature_development
 from RUN_1_acceleration import EXERCISES
+from paths import path_output
 
 #### a ----------------------------------------------------------------------------
 # Load scores_ALL_ex
 scores_ALL_ex = pd.read_csv('scores_ALL_ex.csv')  
 counts = scores_ALL_ex.groupby(["Exercise", "Patient"])["BMI"].count() # enthält keine col 'Exertion'
 df = scores_ALL_ex.groupby(["Exercise", "Patient"]).mean()
-df["counts"] = counts # Aggregated by counts (eg 4 obs) per group.
+df["counts"] = counts # Shows how many counts were aggregated to calculate mean for each group (eg 4 obs).
 
 df = df.reset_index() # ev nötig damit pairplot geht.. Muss hue-variable factor sein?
-df.info()
+# df.info()
 
 
 #### Correlation ----------------------------------------------------------------------------
@@ -31,8 +32,17 @@ df = df.drop('counts', 1) # drop col counts
 
 import seaborn as sns
 sns.set_theme(style="ticks")
+
+# by "Exercise"
 sns.pairplot(df, hue="Exercise")
+path_save = Path(path_output, 'plots/corr_features/corr_features_HUE_exercise.png')
+plt.savefig(path_save)
+
+# by "Patient"
 sns.pairplot(df, hue="Patient")
+path_save = Path(path_output, 'plots/corr_features/corr_features_HUE_patient.png')
+plt.savefig(path_save)
+
 
 
 
--- studies/imove/analysis/acceleration/paths.py
+++ studies/imove/analysis/acceleration/paths.py
@@ -10,6 +10,6 @@ import context # it can oly import context.py when contained in the same folder 
 
 path_data = '/Users/julien/GD/ACLS/TM/DATA/'
 #path_src = '/Users/julien/My Drive/20_STUDIUM/ACLS/05 Module/TM/mhealth/src'
-#path_output = '/Users/julien/My Drive/20_STUDIUM/ACLS/05 Module/TM/OUTPUT'
+path_output = '/Users/julien/My Drive/20_STUDIUM/ACLS/05 Module/TM/OUTPUT'
 #plots_path = '/Users/julien/My Drive/20_STUDIUM/ACLS/05 MODULE/TM/OUTPUT/plots/'
 
